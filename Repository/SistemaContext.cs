98e596debd5324226f0438be80f13ff54838f534
AriadneF <52201142+AriadneF@users.noreply.github.com>
2019-08-23 09:56:55 -0300

    DbSet<Cargo> foi criado no SistemaContext

---
+++ Repository/SistemaContext.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class SistemaContext: DbContext
+    {
+        public SistemaContext() : base("SqlServerConnection") { }
+
+        public DbSet<Paciente> Paciente { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+    }
+}

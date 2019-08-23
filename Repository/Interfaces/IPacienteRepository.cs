bc0caea31f65f1e67ee151948ee14a17a04e6d37
LucasHigert <lucassembarskihigert@gmail.com>
2019-08-23 11:02:17 -0300

    Adicionado Repository e Interface do Paciente

---
+++ Repository/Interfaces/IPacienteRepository.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IPacienteRepository
+    {
+        Paciente ObterPeloId(int id);
+
+        int Inserir(Paciente paciente);
+
+        bool Alterar(Paciente paciente);
+
+        bool Apagar(int id);
+
+        List<Paciente> ObterPacientesPeloIdCidade(int idPaciente);
+
+
+    }
+
+}
---
+++ Repository/Repository/PacienteRepository.cs
@@ -0,0 +1,38 @@
+﻿using Model;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    class PacienteRepository : IPacienteRepository
+    {
+        public bool Alterar(Paciente paciente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Inserir(Paciente paciente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Paciente> ObterPacientesPeloIdCidade(int idPaciente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Paciente ObterPeloId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

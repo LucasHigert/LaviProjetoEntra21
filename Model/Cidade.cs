a8eb8413c16e2c83678140a1ee07693d21edbf2c
Heloisa da Silva Mengisztki <heloisasmegisztki@gmail.com>
2019-08-23 11:35:31 -0300

    Iniciado alterar nos estadosRepository

--- Model/Cidade.cs
+++ Model/Cidade.cs
@@ -22,5 +22,8 @@ namespace Model
 
         [ForeignKey("IdEstado")]
         public Estado Estado { get; set; }
+
+        [Column("registro_ativo")]
+        public bool RegistroAtivo { get; set; }
     }
 }

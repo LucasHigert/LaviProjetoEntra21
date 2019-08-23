9b7f05226604a01b8ed89d2dc6af77f6f254f6ff
Heloisa da Silva Mengisztki <heloisasmegisztki@gmail.com>
2019-08-23 10:36:31 -0300

    Colocado o registro_ativo no banco de dados

--- Repository/Interfaces/IEstadoRepository.cs
+++ Repository/Interfaces/IEstadoRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Interfaces
 {
-    interface IEstado
+    interface IEstadoRepository
     {
         List<Estado> ObterTodos();
 
--- Repository/SistemaContext.cs
+++ Repository/SistemaContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace Repository
         public SistemaContext() : base("SqlServerConnection") { }
 
         public DbSet<Paciente> Paciente { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<Cidade> Cidades { get; set; }
     }
 }

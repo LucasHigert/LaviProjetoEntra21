87c519144c820d42eb6228a7dff7b7f8de35b942
Heloisa da Silva Mengisztki <heloisasmegisztki@gmail.com>
2019-08-23 08:20:59 -0300

    adicionado Entity no Model e Repository

--- Model/Estado.cs
+++ Model/Estado.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Estado
+    public class Estado
     {
-        //Modificado para testes
+        [Table("estados")]
+        public class Pessoa
+        {
+            [Key, Column("id")]
+            public int Id { get; set; }
+
+            [Column("nome")]
+            public string Nome { get; set; }
+
+            [Column("registro_ativo")]
+            public bool RegistroAtivo { get; set; }
+        }
     }
 }

a42eaaff529e0cd5a229aa6a38f96b956b514127
AriadneF <52201142+AriadneF@users.noreply.github.com>
2019-08-23 08:25:58 -0300

    Model Cargos criado

--- Model/Cargos.cs
+++ Model/Cargos.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    class Cargos
+    [Table("cargos")]
+    public class Cargos
     {
+        [Key, Column("id_cargo")]
+        public int IdCargo { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("registro_ativo")]
+        public bool RegistroAtivo { get; set; }
     }
 }

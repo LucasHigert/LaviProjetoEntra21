9db9fedd8b03efc24ff4657b7406218c780fc6e1
ElianNasatto <elian.nasa123@gmail.com>
2019-08-30 11:28:27 -0300

    Alterado design e listagem de atendimentos

---
+++ Repository/Repository/AtendimentoRepository.cs
@@ -0,0 +1,74 @@
+﻿using Model;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class AtendimentoRepository : IAtendimentoRepository
+    {
+        private SistemaContext context = new SistemaContext();
+
+        public bool Alerar(Atendimento atendimento)
+        {
+            var atendimentoOriginal = context.Atendimentos.Where(x => x.Id == atendimento.Id && x.Status != 2).FirstOrDefault();
+            if (atendimentoOriginal == null)
+            {
+                return false;
+            }
+            else
+            {
+                atendimentoOriginal.IdEncaminhamento = atendimento.IdEncaminhamento;
+                atendimentoOriginal.IdFuncionario = atendimento.IdFuncionario;
+                atendimentoOriginal.IdMedico = atendimento.IdMedico;
+                atendimentoOriginal.DataAtendimento = atendimento.DataAtendimento;
+                atendimentoOriginal.Tratamento = atendimento.Tratamento;
+                atendimentoOriginal.Prioridade = atendimento.Prioridade;
+                atendimentoOriginal.Status = atendimento.Status;
+                atendimentoOriginal.Observacao = atendimento.Observacao;
+                atendimentoOriginal.Pressao = atendimento.Pressao;
+
+                var rowsAffected = context.SaveChanges();
+
+                return rowsAffected == 1;
+
+            }
+        }
+
+        public bool Apagar(int id)
+        {
+            var atendimentoOrginal = context.Atendimentos.Where(x => x.Id == id).FirstOrDefault();
+            if (atendimentoOrginal == null)
+            {
+                return false;
+            }
+            else
+            {
+                atendimentoOrginal.Status = 2;
+                var rowsAffected = context.SaveChanges();
+                return rowsAffected == 1;
+            }
+
+        }
+
+        public bool Inserir(Atendimento atendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Atendimento ObterPeloId(int id)
+        {
+            return context.Atendimentos.Where(x => x.Id == id && x.Status != 2).FirstOrDefault();
+        }
+
+
+        public List<Atendimento> ObterTodosPeloCargo(int NumeroCargo)
+        {
+            //modificar para buscar o atendimento pelo cargo que esta logado
+            return context.Atendimentos.Include("Paciente").Where(x => x.Status != 2).OrderByDescending(x=> x.Prioridade).ToList();
+        }
+    }
+}
--- View/Controllers/AtendimentoController.cs
+++ View/Controllers/AtendimentoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,41 @@ namespace View.Controllers
 {
     public class AtendimentoController : Controller
     {
+        private AtendimentoRepository repositoryAtendimento = new AtendimentoRepository();
+        private PacienteRepository repositoryPaciente = new PacienteRepository();
         // GET: Atendimento
         public ActionResult Index()
         {
+            ViewBag.Atendimentos = repositoryAtendimento.ObterTodosPeloCargo(1);
             return View();
+        }
+
+
+        public ActionResult Cadastro()
+        {
+            ViewBag.Pacientes = repositoryPaciente.ObterPacientesPeloIdCidade(1);
+            return View();
+        }
+
+        public ActionResult Inserir(int atendendente,int medico,int paciente,DateTime data,int prioridate,string observacao)
+        {
+            Atendimento atendimento = new Atendimento();
+            atendimento.IdFuncionario = atendendente;
+            atendimento.IdMedico = medico;
+            atendimento.IdPaciente = paciente;
+            atendimento.DataAtendimento = data;
+            atendimento.Prioridade = prioridate;
+            atendimento.Observacao = observacao;
+
+            var inseriu = repositoryAtendimento.Inserir(atendimento);
+            if (inseriu == false)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
     }
 }

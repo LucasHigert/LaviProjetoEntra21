be88dde40e56a0bca35e911b9dabd971977633e1
LucasHigert <lucassembarskihigert@gmail.com>
2019-09-18 08:51:56 -0300

    Contents modificado

---
+++ View/Scripts/Encaminhamento/ModalPosto.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $(".details").click(function () {
+        var id = $(this).attr("data-id");
+        $("#modal").load("Details?id=" + id, function () {
+            $("#modal").modal();
+        })
+    });
+})
+
+
+
+

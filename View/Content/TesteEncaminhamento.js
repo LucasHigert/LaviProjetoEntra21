1aaf432f6aec2bc3a8510bae0002e43d4a7452d3
HeloisaMengisztki <Heloisasmengisztki@gmail.com>
2019-09-19 08:46:32 -0300

    feito o campo dinâmico no js para farmácia

--- View/Content/TesteEncaminhamento.js
+++ View/Content/TesteEncaminhamento.js
@@ -24,3 +24,20 @@ function printElement(elem) {
     $printSection.appendChild(domClone);
     window.print();
 }
+
+function duplicarCampos() {
+    var clone = document.getElementById('origem').cloneNode(true);
+    var destino = document.getElementById('destino');
+    destino.appendChild(clone);
+
+    var camposClonados = clone.getElementsByTagName('input');
+
+    for (i = 0; i < camposClonados.length; i++) {
+        camposClonados[i].value = '';
+    }
+}
+
+function removerCampos(id) {
+    var node1 = document.getElementById('destino');
+    node1.removeChild(node1.childNodes[0]);
+}

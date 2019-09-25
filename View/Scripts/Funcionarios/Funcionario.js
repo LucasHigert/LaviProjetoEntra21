3b8db1861c578d00833e359b7028e23eea283adf
mirianmmweber <mirianweber76@gmail.com>
2019-09-25 10:35:49 -0300

    update butão invisivel em Funcionario

--- View/Scripts/Funcionarios/Funcionario.js
+++ View/Scripts/Funcionarios/Funcionario.js
@@ -2,6 +2,7 @@
 //    function confere() {
 //        if (document.getElementById('campo-nome').value == "" || document.getElementById('campo-login').value == "" || document.getElementById('campo-senha').value == "") {
 //            alert("Por favor, preencha todos os campos");
+//            document.getElementById('botao').style.visibility = "hidden";
 //        } else {
 //            document.getElementById("botao").style.visibility = "visible";
 //        }

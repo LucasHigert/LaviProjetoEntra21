d0ef6a3cf83c12f3371311781aba4bc46d2776b0
mirianmmweber <mirianweber76@gmail.com>
2019-09-25 10:09:28 -0300

    update funcionarios, tentativa de deixar o botão invisivel, ainda precisa de ajustes

---
+++ View/Scripts/Funcionarios/Funcionario.js
@@ -0,0 +1,9 @@
+﻿//$(function () {
+//    function confere() {
+//        if (document.getElementById('campo-nome').value == "" || document.getElementById('campo-login').value == "" || document.getElementById('campo-senha').value == "") {
+//            alert("Por favor, preencha todos os campos");
+//        } else {
+//            document.getElementById("botao").style.visibility = "visible";
+//        }
+//    }
+//});

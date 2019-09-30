7d1c488ba0842da76e98b02152b6902121ed1ccf
HeloisaMengisztki <Heloisasmengisztki@gmail.com>
2019-09-30 11:33:44 -0300

    feito a máscara em pacientes

---
+++ View/Scripts/Mask/Mascaras.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function ($) {
+    $('#campo-cep').mask('00000-000');
+    $('#campo-telefone').mask('(00)90000-0000');
+    $('#campo-cpf').mask('000.000.000-00');
+    $('#campo-rne').mask('A000000-A');
+    $('#campo-passaporte').mask('AA000000');
+    $('#campo-pressao').mask('900/90');
+    $('#campo-peso').mask('900.009');
+    $('#campo-altura').mask('0.00');
+    $('#campo-temperatura').mask('00.0');
+});
+
+//adiciona isso aqui
+
+//<script type="text/javascript" src="~/Scripts/Mask/jquery.mask.min.js"></script>
+//  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>

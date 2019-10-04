284055c0b91528405121a476676294e439f9b554
heloisaMengisztki <heloisasmengisztki@gmail.com>
2019-10-03 23:37:43 -0300

    tentado puxar id do atendimento

--- View/Scripts/Paciente/BuscaPacienteDocScript.js
+++ View/Scripts/Paciente/BuscaPacienteDocScript.js
@@ -3,31 +3,31 @@
     $controle = 0;
     function AtualizaTabela() {
         if ($controle == 0) {
-        $tabelaAtendimento = $('#tabelaAtendimento').DataTable({
-            ajax: {
-                url: "/paciente/ObterPeloPaciente",
-                data: function (d) {
-                    d.id = $("#campo-id").val()
-                }
-            },
-            method: "GET",
-            serverSide: true,
-            searching: false,
-            info: false,
-            paging: false,
-            columns: [
-                {
-                    render: function (data, type, row) {
-                        return moment(row).format("DD/MM/YYYY")
+            $tabelaAtendimento = $('#tabelaAtendimento').DataTable({
+                ajax: {
+                    url: "/paciente/ObterPeloPaciente",
+                    data: function (d) {
+                        d.id = $("#campo-id").val()
                     }
                 },
-                {
-                    render: function (data, type, row) {
-                        return '<a class="botao-atendimento btn btn-info" id=' + row.Id + '"\   data-id="' + row.Id + '"\
+                method: "GET",
+                serverSide: true,
+                searching: false,
+                info: false,
+                paging: false,
+                columns: [
+                    {
+                        render: function (data, type, row) {
+                            return moment(row).format("DD/MM/YYYY")
+                        }
+                    },
+                    {
+                        render: function (data, type, row) {
+                            return '<a class="botao-atendimento btn btn-info" id=' + row.Id + '"\   data-id="' + row.Id + '"\
                     data-nome="' + row.Nome + '"><i class="fa fa-user"></i></a>'
+                        }
                     }
-                }
-            ]
+                ]
             });
             $controle = 1;
         } else {
@@ -41,8 +41,29 @@
         AtualizaTabela();
     });
 
-    $("#tabelaAtendimento").on("click", ".botao-atendimento", function () {
-        $('#documentoFormularioModal').modal('show');
+
+    $("#documentoFormularioModal").on('shown.bs.modal', function () {
+        $("#campo-idAtendimento").val($idPaciente)
+        $('#tabelaSintomas').DataTable({
+            ajax: {
+                url: "/atendimentoespecial/ObterSintomaAtendimento?idAtendimento=" + $idPaciente,
+            },
+            method: "GET",
+            serverSide: true,
+            searching: false,
+            info: false,
+            paging: false,
+            columns: [
+                {
+                    "data": "sintoma",
+                    "data": "intensidade"
+                }
+            ]
+        });
     });
 
-})
+    $("#tabelaAtendimento").on("click", ".botao-atendimento", function () {
+        $('#documentoFormularioModal').modal('show');
+        $idPaciente = $("#id").val(this);
+    })
+});
